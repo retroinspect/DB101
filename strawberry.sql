ea43fecf0832ed0b9912e816b3846ee44682e585
sanga <sangachoi2103@gmail.com>
2020-04-18 14:24:08 +0900

    erm added

--- strawberry.sql
+++ strawberry.sql
@@ -3,11 +3,8 @@ SET SESSION FOREIGN_KEY_CHECKS=0;
 /* Drop Tables */
 
 DROP TABLE IF EXISTS DemandComment;
-DROP TABLE IF EXISTS UserDemandPostLikes;
+DROP TABLE IF EXISTS UserPostLikes;
 DROP TABLE IF EXISTS DemandPost;
-DROP TABLE IF EXISTS SupplyComment;
-DROP TABLE IF EXISTS UserSupplyPostLikes;
-DROP TABLE IF EXISTS SupplyPost;
 DROP TABLE IF EXISTS Categories;
 DROP TABLE IF EXISTS User;
 
@@ -19,7 +16,7 @@ DROP TABLE IF EXISTS User;
 CREATE TABLE Categories
 (
 	CategoryTitle varchar(20) NOT NULL,
-	CategoryId int unsigned NOT NULL AUTO_INCREMENT,
+	CategoryId int NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (CategoryId),
 	UNIQUE (CategoryTitle),
 	UNIQUE (CategoryId)
@@ -28,90 +25,53 @@ CREATE TABLE Categories
 
 CREATE TABLE DemandComment
 (
-	DemandPostId int unsigned NOT NULL,
-	DemandCommentId int unsigned NOT NULL AUTO_INCREMENT,
-	UserId int unsigned NOT NULL,
-	CreatedDate timestamp NOT NULL,
-	PRIMARY KEY (DemandCommentId),
-	UNIQUE (DemandCommentId)
+	DemandPostId int NOT NULL,
+	DemandCmtId int NOT NULL AUTO_INCREMENT,
+	UserId int NOT NULL,
+	CreatedDate ,
+	PRIMARY KEY (DemandCmtId),
+	UNIQUE (DemandCmtId)
 );
 
 
 CREATE TABLE DemandPost
 (
-	DemandPostId int unsigned NOT NULL AUTO_INCREMENT,
-	CreatedUserId int unsigned NOT NULL,
-	PostCategoryId int unsigned NOT NULL,
-	Nego boolean DEFAULT 'TRUE' NOT NULL,
-	ImgSrc varchar(200),
-	Description varchar(500),
-	numLikes int unsigned zerofill NOT NULL,
-	DemandPostTitle varchar(20) NOT NULL,
-	DemandPostLocation varchar(20) NOT NULL,
-	DemandPostPrice int unsigned,
-	DemandPostCreatedDate timestamp NOT NULL,
+	DemandPostId int NOT NULL AUTO_INCREMENT,
+	CreatedUserId int NOT NULL,
+	PostCategoryId int NOT NULL,
+	Nego ,
+	ImgSrc ,
+	Description ,
+	numLikes ,
+	DemandPostID ,
+	DemandPostTitle ,
+	DemandPostLocation ,
+	DemandPostPrice ,
+	DemandPostCreatedDate ,
 	PRIMARY KEY (DemandPostId),
 	UNIQUE (DemandPostId)
 );
 
 
-CREATE TABLE SupplyComment
-(
-	SupplyCommentId int NOT NULL AUTO_INCREMENT,
-	CreatedDate timestamp NOT NULL,
-	SupplyPostID int unsigned NOT NULL,
-	UserId int unsigned NOT NULL,
-	PRIMARY KEY (SupplyCommentId),
-	UNIQUE (SupplyCommentId)
-);
-
-
-CREATE TABLE SupplyPost
-(
-	SupplyPostID int unsigned NOT NULL AUTO_INCREMENT,
-	ImgSrc varchar(200) NOT NULL,
-	Nego boolean DEFAULT 'TRUE' NOT NULL,
-	Description varchar(500),
-	numLikes int unsigned zerofill NOT NULL,
-	Title varchar(20) NOT NULL,
-	Location varchar(20) NOT NULL,
-	Price int unsigned NOT NULL,
-	CreatedDate timestamp NOT NULL,
-	ThingState varchar(20) NOT NULL,
-	CategoryId int unsigned NOT NULL,
-	PRIMARY KEY (SupplyPostID),
-	UNIQUE (SupplyPostID)
-);
-
-
 CREATE TABLE User
 (
-	UserId int unsigned NOT NULL AUTO_INCREMENT,
-	UserName varchar(20) NOT NULL,
-	UserRep tinyint DEFAULT 5 NOT NULL,
-	UserPhoneNum varchar(20) NOT NULL,
-	UserPW varchar(30) NOT NULL,
-	UserGender varchar(20) NOT NULL,
-	UserEmail varchar(30),
-	UserLocation varchar(20) NOT NULL,
-	UserNickname varchar(20) NOT NULL,
-	PRIMARY KEY (UserId),
-	UNIQUE (UserId)
+	UserId int NOT NULL AUTO_INCREMENT,
+	UserName  NOT NULL,
+	UserRep ,
+	UserPW ,
+	UserGender ,
+	UserEmail ,
+	UserPhoneNum ,
+	UserLocation ,
+	UserNickname ,
+	PRIMARY KEY (UserId)
 );
 
 
-CREATE TABLE UserDemandPostLikes
+CREATE TABLE UserPostLikes
 (
-	UserId int unsigned NOT NULL,
-	DemandPostId int unsigned NOT NULL
-);
-
-
-CREATE TABLE UserSupplyPostLikes
-(
-	UserId int unsigned NOT NULL,
-	SupplyPostID int unsigned NOT NULL,
-	UNIQUE (SupplyPostID)
+	UserId int NOT NULL,
+	DemandPostId int NOT NULL
 );
 
 
@@ -126,14 +86,6 @@ ALTER TABLE DemandPost
 ;
 
 
-ALTER TABLE SupplyPost
-	ADD FOREIGN KEY (CategoryId)
-	REFERENCES Categories (CategoryId)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
 ALTER TABLE DemandComment
 	ADD FOREIGN KEY (DemandPostId)
 	REFERENCES DemandPost (DemandPostId)
@@ -142,25 +94,9 @@ ALTER TABLE DemandComment
 ;
 
 
-ALTER TABLE UserDemandPostLikes
+ALTER TABLE UserPostLikes
 	ADD FOREIGN KEY (DemandPostId)
 	REFERENCES DemandPost (DemandPostId)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE SupplyComment
-	ADD FOREIGN KEY (SupplyPostID)
-	REFERENCES SupplyPost (SupplyPostID)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE UserSupplyPostLikes
-	ADD FOREIGN KEY (SupplyPostID)
-	REFERENCES SupplyPost (SupplyPostID)
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
@@ -182,23 +118,7 @@ ALTER TABLE DemandPost
 ;
 
 
-ALTER TABLE SupplyComment
-	ADD FOREIGN KEY (UserId)
-	REFERENCES User (UserId)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE UserDemandPostLikes
-	ADD FOREIGN KEY (UserId)
-	REFERENCES User (UserId)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE UserSupplyPostLikes
+ALTER TABLE UserPostLikes
 	ADD FOREIGN KEY (UserId)
 	REFERENCES User (UserId)
 	ON UPDATE RESTRICT
